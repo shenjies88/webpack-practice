389c25a7902fac416180c6aee01dbc94090141e9
shenjies88 <1961486254@qq.com>
2020-02-09 10:37:05 +0800

    feat:基础搭建

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,35 @@ module.exports = {
                     'css-loader',
                 ],
             },
+            {
+                test: /\.less$/i,
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.jsx?/i, // 支持 js 和 jsx
+                include: [
+                    path.resolve(__dirname, 'src'), // src 目录下的才需要经过 babel-loader 处理
+                ],
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
         ],
     }
-}
+};
